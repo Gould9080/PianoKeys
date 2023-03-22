5247308496c3151dc5e0471c6229919f2f8a2ee2
James Craig <james.edward.craig@gmail.com>
2023-03-22 18:27:26 -0400

    added note display, cleaned up js

--- js.js
+++ js.js
@@ -1,30 +1,32 @@
-const c4 = document.querySelector('#c4');
-const c4s = document.querySelector('#c4s');
-const d4 = document.querySelector('#d4');
-const d4s = document.querySelector('#d4s');
-const e4 = document.querySelector('#e4');
-const f4 = document.querySelector('#f4');
-const f4s = document.querySelector('#f4s');
-const g4 = document.querySelector('#g4');
-const g4s = document.querySelector('#g4s');
-const a4 = document.querySelector('#a4');
-const a4s = document.querySelector('#a4s');
-const b4 = document.querySelector('#b4');
-const c5 = document.querySelector('#c5');
-const c5s = document.querySelector('#c5s');
-const d5 = document.querySelector('#d5');
-const d5s = document.querySelector('#d5s');
-const e5 = document.querySelector('#e5');
-const f5 = document.querySelector('#f5');
-const f5s = document.querySelector('#f5s');
-const g5 = document.querySelector('#g5');
-const g5s = document.querySelector('#g5s');
-const a5 = document.querySelector('#a5');
-const a5s = document.querySelector('#a5s');
-const b5 = document.querySelector('#b5');
-const c6 = document.querySelector('#c6');
-// write a function that defines these variables?
+/* Frequencies of notes:
 
+C4 - 262
+C#4 - 277
+D4 - 294
+D#4 - 311
+E4 - 330
+F4 - 349
+F#4 - 370
+G4 - 392
+G#4 - 415
+A4 - 440
+A#4 - 466
+B4 - 494
+c5 - 523
+c#5 - 554
+d5 - 587
+d#5 - 622
+e5 - 659
+f5 - 698
+f#5 - 740
+g5 - 784
+g#5 - 831
+a5 - 880
+a#5 - 932
+b5 - 988
+C6 - 1047
+
+*/
 
 //https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
 var audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
@@ -45,7 +47,7 @@ function beep(frequency, duration = 250, volume, type, callback) {
     oscillator.stop(audioCtx.currentTime + ((duration || 500) / 1000));
 };
 
-// for keydown / keyup response
+// for keydown / keyup response, not used yet
 function beepStart(frequency) {
     var oscillator = audioCtx.createOscillator();
     var gainNode = audioCtx.createGain();
@@ -59,51 +61,62 @@ function beepStart(frequency) {
 
 
 
-// C4 - 262
-// C#4 - 277
-// D4 - 294
-// D#4 - 311
-// E4 - 330
-// F4 - 349
-// F#4 - 370
-// G4 - 392
-// G#4 - 415
-// A4 - 440
-// A#4 - 466
-// B4 - 494
-// c5 - 523
-// c#5 - 554
-// d5 - 587
-// d#5 - 622
-// e5 - 659
-// f5 - 698
-// f#5 - 740
-// g5 - 784
-// g#5 - 831
-// a5 - 880
-// a#5 - 932
-// b5 - 988
-// C6 - 1047
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // When the DOM Content has loaded attach a click listener
+
+
     let keys = document.querySelectorAll('.keys');
     keys.forEach(k => {
-        k.addEventListener('click', (event) => {
+        k.addEventListener('mousedown', (event) => {
             let target = event.target;
+            originalText = target.innerHTML;
             let frequency = setFrequency(target.id);
-            console.log(target.id);
+            // console.log(target.id);
             beep(frequency);
+            event.target.classList.add('played');
+            let noteName = target.id[0];
+            target.innerHTML = `${noteName}`   //target.id
+        });
+
+        k.addEventListener('mouseup', (event) => {
+            let target = event.target;
+            event.target.classList.remove('played');
+            target.innerHTML = originalText;
         });
     });
 
+    let originalText; // stores the value of the innerText
     document.addEventListener('keydown', function (event) {
-        console.log(event.key);
+        let note = setKeyToNote(event.key); // returns id of the Div
+        let frequency = setFrequency(note);
+        let notePlayed = document.getElementById(note); // actual Div
+        if (notePlayed) {
+            originalText = notePlayed.innerText;
+        }
+        //console.log(event.key); <- actual key I am pressing
+
+        if (frequency) {
+            beep(frequency);
+            notePlayed.classList.add('played');
+            if (notePlayed.classList.contains('blackKeys')) {
+                notePlayed.innerText = `${note[0]}#`
+            } else {
+                notePlayed.innerText = `${note[0]}`
+            }
+        }
+    })
+
+    document.addEventListener('keyup', function (event) {
         let note = setKeyToNote(event.key);
         let frequency = setFrequency(note);
-        beep(frequency);
+        let notePlayed = document.getElementById(note);
+        if (frequency) {
+            notePlayed.classList.remove('played');
+            notePlayed.innerHTML = originalText;
+        }
     })
+
 });
 
 
@@ -202,11 +215,6 @@ function setKeyToNote(key) {
             return "e5"
     }
 }
-
-
-
-
-
 
 
 
